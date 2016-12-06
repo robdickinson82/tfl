dc43a36035b7ff322ace937b4299607adb64ac9b
Rob Dickinson <Rob Dickinson>
2016-12-06 13:54:47 +0000

    Fixed tests for changed function name

--- TflSession/tests.py
+++ TflSession/tests.py
@@ -66,7 +66,7 @@ class TestBasicTests(unittest.TestCase):
         card = self.get_a_card(user)
         refunds = user.session.card.refund.get_all(card.pi_ref)
         refund = refunds[refunds.keys()[0]]
-        output = user.session.card.refund.start_apply(refund.t_id, refund.journey_id, refund.travelDayKey)
+        output = user.session.card.refund.start_application(refund.t_id, refund.journey_id, refund.travelDayKey)
         logging.info(output)
 
     def get_a_card(self, user):
