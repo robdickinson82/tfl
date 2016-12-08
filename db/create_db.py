ca496c7c0c3d78509fa3ce743b6e41b758b1f0a7
Rob Dickinson <Rob Dickinson>
2016-12-08 17:25:23 +0000

    CReated script to build the db

---
+++ db/create_db.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+import tflDb
+
+tflDb.db.create_all()
---
+++ db/migrations/versions/6ff0c12b35a9_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 6ff0c12b35a9
+Revises: 
+Create Date: 2016-12-08 17:15:33.662000
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6ff0c12b35a9'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
